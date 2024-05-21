74fa9d8801a6d80d24c489ec9b68c052efee62ac
vinayrajgambo <you@example.com>
2024-05-21 11:10:08 +0530

    setup commit

--- init_setup.sh
+++ init_setup.sh
@@ -4,7 +4,7 @@ echo [$(date)]: "START"
 echo [$(date)]: "creating env with python 3.8 version" 
 
 
-conda create --prefix ./env python=3.8 -y
+Conda create --prefix ./env python=3.8 -y
 
 
 echo [$(date)]: "activating the environment" 
